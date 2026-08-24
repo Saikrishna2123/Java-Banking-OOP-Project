a54c658522a28b26aef38bc53b398f21c8ecec83
Gajavada SaiKrishna <ashokgajawada49@gmail.com>
2026-08-24 15:54:18 +0530

    Add Java Banking OOP project

---
+++ src/main/java/com/bank/BankingOopApplication.java
@@ -0,0 +1,13 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingOopApplication.class, args);
+	}
+
+}
